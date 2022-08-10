403b41d9d5191964140a85c39ba70d51986df702
Yi Wang <yi.wang.2005@gmail.com>
2022-08-10 19:44:14 +0000

    Add pipe-gpu.py

---
+++ pipe-gpu.py
@@ -0,0 +1,23 @@
+# This is basically the example program https://pytorch.org/docs/stable/pipeline.html#pipe-apis-in-pytorch
+# tailored for running CPU.  So, we can run it on macOS.
+
+import torch
+from torch.distributed.pipeline.sync import Pipe
+import os
+
+# Need to initialize RPC framework first.
+os.environ["MASTER_ADDR"] = "localhost"
+os.environ["MASTER_PORT"] = "29500"
+torch.distributed.rpc.init_rpc("worker", rank=0, world_size=1)
+
+assert torch.cuda.device_count() >= 2  # We will use two GPUs.
+
+# Build pipe.
+fc1 = torch.nn.Linear(16, 8).cuda(0)
+fc2 = torch.nn.Linear(8, 4).cuda(1)
+model = torch.nn.Sequential(fc1, fc2)
+
+model = Pipe(model, chunks=8)
+input = torch.rand(16, 16).cuda(0)
+output_rref = model(input)
+assert output_rref.to_here().size() == torch.Size([16, 4])
